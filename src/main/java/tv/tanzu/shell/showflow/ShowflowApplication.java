bb2ac334ebedff46c22e0e6079adc55b961ca16e
dashaun <dashaun@dashaun.com>
2023-01-04 02:53:10 -0600

    initilizr with shell and native, sdk env init with java 17 and graalvm

---
+++ src/main/java/tv/tanzu/shell/showflow/ShowflowApplication.java
@@ -0,0 +1,13 @@
+package tv.tanzu.shell.showflow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShowflowApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShowflowApplication.class, args);
+	}
+
+}
---
+++ src/test/java/tv/tanzu/shell/showflow/ShowflowApplicationTests.java
@@ -0,0 +1,13 @@
+package tv.tanzu.shell.showflow;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShowflowApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
